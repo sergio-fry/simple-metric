dcb0c49305549a87b72e4a562956ebb3d3aeb625
Alexander Polyakov <backstabe@gmail.com>
2023-03-11 23:20:07 +0400

    test: rm helper test

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ gem 'rake'
 
 group :test do
   gem 'database_cleaner-active_record'
-  gem 'htmlcompressor', '~> 0.4.0'
   gem 'rspec', '~> 3.10'
   gem 'simplecov', '~> 0.22', require: false
   gem 'timecop', '~> 0.9.6'
@@ -23,4 +22,3 @@ end
 
 # Specify your gem's dependencies in simple-metric.gemspec
 gemspec
-
--- spec/lib/simple_metric/helper_spec.rb
+++ spec/lib/simple_metric/helper_spec.rb
@@ -4,14 +4,10 @@ RSpec.describe SimpleMetric::Helpers do
   let(:dummy_class) do
     Class.new do
       extend SimpleMetric::Helpers
-      extend ActionView::Helpers
-      extend ActionView::Context
     end
   end
 
   describe '.simple_metric_graph' do
-    let(:html_file) { File.read(Rails.root.join('../fixtures/tags.html')) }
-
     before do
       Timecop.freeze(Time.local(2023, 2, 8, 0, 0, 0)) do
         SimpleMetric::Metric.add_data_point('Test Key', Time.now, 10)
@@ -20,13 +16,6 @@ RSpec.describe SimpleMetric::Helpers do
 
     it 'return blank if no metrics' do
       expect(dummy_class.simple_metric_graph).to eq('')
-    end
-
-    it 'return blank if empty array' do
-      out_html = dummy_class.simple_metric_graph('Test Key')
-
-      expect(HtmlCompressor::Compressor.new.compress(out_html))
-        .to eq(HtmlCompressor::Compressor.new.compress(html_file))
     end
   end
 end
