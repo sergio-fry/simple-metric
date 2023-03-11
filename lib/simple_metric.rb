17a0ce805a1a20239cef903ffb80700f929a7f55
Alexander Polyakov <backstabe@gmail.com>
2023-03-11 23:03:00 +0400

    style: rm unused require

--- lib/simple_metric.rb
+++ lib/simple_metric.rb
@@ -1,9 +1,7 @@
 require 'simple_metric/version'
-# require 'simple_metric/data_point'
 require 'simple_metric/data_set'
 require 'simple_metric/helpers'
 require 'simple_metric/metric'
-# require 'ostruct'
 
 module SimpleMetric
   module Rails
